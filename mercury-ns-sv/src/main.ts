56d508d4fd167897413ee536ec145bac49a4b0e0
Pablo <duran.pablo.c@gmail.com>
2020-05-02 22:33:45 -0400

    Swagger implementation

--- mercury-ns-sv/src/main.ts
+++ mercury-ns-sv/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express'; 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Mercury Nestjs Server')
+    .setDescription('Mercury API is designed for supporting arduino wireless devices connected to LAN')
+    .setVersion('0.1')
+    .addTag('mercury')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
